af20abf5edd5e088a462949d2cc3fae53cf735c2
Reese Kneeland <reesekrocks@gmail.com>
2021-03-25 18:32:31 -0500

    refining possible move list functions

--- chess_driver.py
+++ chess_driver.py
@@ -3,9 +3,11 @@ import sys
 from funcs import *
 
 board = chess.Board()
-evalBoard(board)
+gameState = 0
 #main input loop
 while(True):
+    if(gameState == 0):
+        print("Welcome to MiniMax Chess! Please enter the type of game you would like to play, 1 for singleplayer, 2 to play vs an AI, and 3 to have the AI play itself")
     raw_input = ""
     try:
         raw_input = input(">>> ")
@@ -15,17 +17,36 @@ while(True):
     except:
         print("\nGracefully crashing...")
         raw_input = "exit"
-    if(raw_input == ""): continue
-    elif(raw_input == "undo"):
-        undoMove(board)
-    elif(raw_input == "exit" or raw_input == "quit"):
-        sys.exit()
-    else:
-        if(makeMove(board, raw_input) == 1):
-            pass
+    if(raw_input == "exit" or raw_input == "quit"):
+            sys.exit()
+    if(gameState > 0):
+        if(raw_input == "undo"):
+            undoMove(board)
+        if(raw_input == "reset"):
+            resetBoard(board)
         else:
-            continue
-    evalBoard(board)
+            if(makeMove(board, raw_input) == 1):
+                pass
+            else:
+                continue
+            evalBoard(board)
+    if(gameState <= 0):
+        if(raw_input == "1"): 
+            gameState = 1
+            print("You have chosen singleplayer!")
+            evalBoard(board)
+        elif(raw_input == "2"): 
+            gameState = 2
+            print("You have chosen to play against an AI!")
+            evalBoard(board)
+        elif(raw_input == "3"): 
+            gameState = 3
+            print("You have chosen to watch the AI play itself!")
+            evalBoard(board)
+        else:
+            gameState = -1
+            print("That is not a recognized gamemode! Please try again.")
+    if(raw_input == ""): continue
     
 print("Exit")
 
--- funcs.py
+++ funcs.py
@@ -1,35 +1,53 @@
 import chess
 
 def makeMove(board, sanStr):
-    moveStr = str(board.parse_san(sanStr))
-    print(moveStr)
-    move = chess.Move.from_uci(moveStr)
+    try:
+        moveStr = str(board.parse_san(sanStr))
+        move = chess.Move.from_uci(moveStr)
+    except:
+        print("Move not legal, try again. The legal moves for " + getCurPlayer(board) + " are : " + str(getMoveList(board)))
+        return 0
     if(move in board.legal_moves):
         board.push(move)
         return 1
     else:
-        print("Move not legal, try again. The legal moves are: " + str(board.legal_moves))
+        print("Move not legal, try again. The legal moves for " + getCurPlayer(board) + " are : " + str(getMoveList(board)))
         return 0
 
+def getMoveList(board):
+    moveStrList = str(board.legal_moves)
+    moveStrList = moveStrList[39:-2]
+    moveList = moveStrList.split(", ")
+    return moveList
+
+def getCurPlayer(board):
+    if(board.turn == chess.WHITE):
+        return "white"
+    else:
+        return "black"
+
+def getNextPlayer(board):
+    if(board.turn == chess.WHITE):
+        return "black"
+    else:
+        return "white"
 
 def undoMove(board):
     board.pop()
     print("Move undone")
 
+def resetBoard(board):
+    board.reset()
+    print("Board has been reset")
+
 def evalBoard(board):
     print("\nblack\n---------------")
     print(board)
     print("---------------\nwhite\n")
-    if(board.turn == chess.WHITE):
-        curPlayer = "white"
-        nextPlayer = "black"
-    else:
-        curPlayer = "black"
-        nextPlayer = "white"
     
     if(board.is_checkmate()):
-        print(curPlayer + " has been checkmated! " + nextPlayer + " wins!")
+        print(curPlayer + " has been checkmated! " + getNextPlayer(board) + " wins!")
     elif(board.is_check()):
-        print(curPlayer + " is in check! Possible moves are: " + str(board.legal_moves))
+        print(curPlayer + " is in check! Possible moves are: " + str(getMoveList(board)))
     else:
-        print("Possible moves for " + curPlayer + ": " + str(board.legal_moves))
+        print("Possible moves for " + getCurPlayer(board) + ": " + str(getMoveList(board)))
