c6287aef49215f1b432a18ebb115b03ea0fe7940
unknown <ravic038@umn.edu>
2022-04-21 19:36:36 -0500

    Upated parser

--- processData.py
+++ processData.py
@@ -4,15 +4,18 @@ import math
 import numpy as np
 import csv
 
-games=[]
-with open('data/2021_data.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        games.append(row)
-total_heurarray=[]
-count=0
-for game in games:
-    heurArray = generateGameHeuristics(game)
-    count+=1
-    total_heurarray.append(heurArray)
+def heuristics_for_game(fname='data/2021_data.csv'):
+    games=[]
+    with open(fname, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            games.append(row)
+    total_heurarray=[]
+    count=0
+    for game in games:
+        heurArray = generateGameHeuristics(game)
+        count+=1
+        total_heurarray.append(heurArray)
+    return(total_heurarray)
+
 
