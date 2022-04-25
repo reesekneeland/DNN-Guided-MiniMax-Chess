e2ed3bd4e3d58b5b26b740293be30d7f9d2a5ea7
Jonas <jonas060@d.umn.edu>
2022-04-24 19:15:09 -0700

    Cleaned up code added and elaborated with comments

--- nn_prediction.py
+++ nn_prediction.py
@@ -1,19 +1,21 @@
 import torch
 import numpy as np
 
-def nn_prediction(fen_map): #Assumption is input will be board representation in "bitmap" format of post all possible moves. Example: 9 legal moves
-                              #Therefore array of 9 values passed in. Each value being a bitmap of he board after possible move is taken.
+def nn_prediction(fen_map): #input of legal move passed in an fen format. Prediction of advantage gain or lossed predicted and returned
+                            #range for move prediction outputted is [-15, 15].
     model = torch.load('entire_model.pth') #Determine how we can load in the neural network model.
     model.eval() # Set model to eval mode
     
     with torch.no_grad(): # Deactivate gradients for the following code
         
-        split = fen_map.split(" ")
-        mapping = split[0]
-        mapping = mapping.encode('utf-8')
+        split = fen_map.split(" ") #Split fen mapping
+        mapping = split[0] #Retrieve mapping of pieces on chess board only
+        mapping = mapping.encode('utf-8') #encode fen mapping of chess board only in utf-8
         bin = np.frombuffer(mapping, dtype=np.uint8)
-        bin = np.unpackbits(bin, axis=0).astype(np.single)
-        array = np.zeros(808)
+        bin = np.unpackbits(bin, axis=0).astype(np.single) #unpack utf-8 format of fen mapping of chess board only
+        array = np.zeros(808) #byte length of unpacked fen mapping of chessboard only will vary depending upon how many pieces are left
+                              #in game. Fen mapping of chessboard will never exceed 808 bytes therefore we employ 808 bytes as our input
+                              #to the neural network.
 
         for value in range(len(array)):
             try:
@@ -21,7 +23,6 @@ def nn_prediction(fen_map): #Assumption is input will be board representation in
             except:
                 break
         
-        #cleaned_data, data_labels = cleaned_data.to(device), data_labels.to(device)
         preds = model(array)
         prediction = preds[0]
 
